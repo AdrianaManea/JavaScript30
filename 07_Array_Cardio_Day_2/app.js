8c5fcbccd3b577d04dc9b4fc4e11e0cd1662cdc8
Adina <adrianna.manea@gmail.com>
2021-02-08 18:42:23 +0100

    07

--- 07_Array_Cardio_Day_2/app.js
+++ 07_Array_Cardio_Day_2/app.js
@@ -39,13 +39,75 @@ const comments = [{
 ];
 
 // Some and Every Checks
-// Array.prototype.some() // is at least one person 19 or older?
+// Array.prototype.some() - check's if at least one thing in your array meets what you're looking for
+// is at least one person 19 or older?
+
+// const isAdult = people.some(function (person) {
+//   const currentYear = (new Date()).getFullYear();
+//   if (currentYear - person.year >= 19) {
+//     return true;
+//   }
+// });
+
+// same thing as above but cleaner - 01
+// const isAdult = people.some(person => {
+//   const currentYear = (new Date()).getFullYear();
+//   return currentYear - person.year >= 19;
+// });
+
+// same thing as above but cleaner, even shorter - 02
+const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+console.log({
+  isAdult
+});
+
+
+
 // Array.prototype.every() // is everyone 19 or older?
+const allAdult = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+console.log({
+  allAdult
+});
+
+
 
 // Array.prototype.find()
-// Find is like filter, but instead returns just the one you are looking for
+// Find is like filter(), but instead of returning you a subset of the array it returns the first item you are looking for/that it finds
+
 // find the comment with the ID of 823423
+// const comment = comments.find(function (comment) {
+//   if (comment.id === 823423) {
+//     return true;
+//   }
+// });
+
+// same thing as above but cleaner
+const comment = comments.find(comment => comment.id === 823423);
+
+console.log(comment);
+
+
 
 // Array.prototype.findIndex()
+// find() where something is inside of an array
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+const index = comments.findIndex(comment => comment.id === 823423);
+
+console.log(index);
+
+// How do you delete it?
+// 1
+comments.splice(index, 1);
+
+// 2
+// Create a new Array of the updated comments
+const newComments = [
+  ...comments.slice(0, index),
+  ...comments.slice(index + 1)
+];
+
+console.table(comments);
+console.table(newComments);

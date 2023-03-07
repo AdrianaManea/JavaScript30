69a23290bd07558fda028a9dd80576340c0250b4
Adriana Manea <adrianna.manea@gmail.com>
2023-03-07 15:18:57 +0100

    add comments

---
+++ 01_JavaScript_Funny_Sounds/README.md
@@ -0,0 +1,16 @@
+
+## keycode.info
+
+
+## Learned about:
+ - data attributes - to hook up [data-key="keycode"] from the key/div along with [data-key="keycode"] from the audio
+ - key events
+ - playing audio
+ - listen for the 'transitionend' event
+ - you can listen for the 'animationend' event too
+
+
+ ## What it does:
+  - Hit the corresponding key on your keyboard is doing 2 things:
+    - plays the song associated with the key
+    - animation - scale the button, apply yellow border, change box-shadow
--- 01_JavaScript_Funny_Sounds/app.js
+++ 01_JavaScript_Funny_Sounds/app.js
@@ -1,9 +1,17 @@
 function playSound(e) {
+  // console.log(e);
+  // console.log(e.keyCode);
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  
+  // console.log(key);
+  
+  // if no audio then
+  // stop the function from running all together
+  if (!audio) return; 
 
-  if (!audio) return; // stop the function from running all together
   audio.currentTime = 0; // rewind to the start
+
   audio.play();
 
   key.classList.add('playing');
@@ -11,12 +19,16 @@ function playSound(e) {
 
 function removeTransition(e) {
   // console.log(e);
+  // console.log(e.propertyName);
+
   if (e.propertyName !== 'transform') return; // skip it if it's not a transform
   // console.log(e.propertyName);
+  
   // console.log(this);
   this.classList.remove('playing');
 }
 
+// Use a transition end event that will fire when it stopped animating itself
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
